d935bc3b42b32572c27fb32156c9cbb7b15015c1
GalBrandwine <gal080592@gmail.com>
2021-07-09 20:48:47 +0300

    Minor refactoring and Adding setYear to html

--- config/RTCconfigurations.ino
+++ config/RTCconfigurations.ino
@@ -11,36 +11,39 @@ const int RTCsettingMonthday = 7;
 const int RTCsettingMonth = 4;
 const int RTCsettingYear = 2021;
 
+SkyNetStruct sky_net;
+
 void setup()
 {
   Serial.begin(115200);
   // RTC initiations
   Wire.begin();
   DS3231_init(DS3231_CONTROL_INTCN);
-  rtc_timestamp.hour = RTCsettingHour;
-  rtc_timestamp.min = RTCsettingMin;
-  rtc_timestamp.sec = RTCsettingSec;
-  rtc_timestamp.mday = RTCsettingMonthday;
-  rtc_timestamp.mon = RTCsettingMonth;
-  rtc_timestamp.year = RTCsettingYear;
-  DS3231_set(rtc_timestamp);
+
+  sky_net.rtc_timestamp.hour = RTCsettingHour;
+  sky_net.rtc_timestamp.min = RTCsettingMin;
+  sky_net.rtc_timestamp.sec = RTCsettingSec;
+  sky_net.rtc_timestamp.mday = RTCsettingMonthday;
+  sky_net.rtc_timestamp.mon = RTCsettingMonth;
+  sky_net.rtc_timestamp.year = RTCsettingYear;
+  DS3231_set(sky_net.rtc_timestamp);
 }
 
 void loop()
 {
-  DS3231_get(&rtc_timestamp);
+  DS3231_get(&sky_net.rtc_timestamp);
   Serial.print("Date : ");
-  Serial.print(rtc_timestamp.mday);
+  Serial.print(sky_net.rtc_timestamp.mday);
   Serial.print("/");
-  Serial.print(rtc_timestamp.mon);
+  Serial.print(sky_net.rtc_timestamp.mon);
   Serial.print("/");
-  Serial.print(rtc_timestamp.year);
+  Serial.print(sky_net.rtc_timestamp.year);
   Serial.print("\t Hour : ");
-  Serial.print(rtc_timestamp.hour);
+  Serial.print(sky_net.rtc_timestamp.hour);
   Serial.print(":");
-  Serial.print(rtc_timestamp.min);
+  Serial.print(sky_net.rtc_timestamp.min);
   Serial.print(".");
-  Serial.println(rtc_timestamp.sec);
+  Serial.println(sky_net.rtc_timestamp.sec);
 
   delay(1000);
 }
--- include/setUpServer.h
+++ include/setUpServer.h
@@ -5,28 +5,32 @@
 bool setUpServer(SkyNetStruct *skyNetStruct)
 {
   // Route for root / web page
-  skyNetStruct->server->on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/index.html", String(), false);
-    Serial.println("Got HTTP_GET on '/'");
-  });
+  skyNetStruct->server->on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+                           {
+                             request->send(SPIFFS, "/index.html", String(), false);
+                             Serial.println("Got HTTP_GET on '/'");
+                           });
 
   // Route to load style.css file
-  skyNetStruct->server->on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/style.css", "text/css");
-    Serial.println("Got HTTP_GET on '/style.css'");
-  });
+  skyNetStruct->server->on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
+                           {
+                             request->send(SPIFFS, "/style.css", "text/css");
+                             Serial.println("Got HTTP_GET on '/style.css'");
+                           });
 
   // Route to load clout_1.jpg image
-  skyNetStruct->server->on("/cloud_1.jpg", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/cloud_1.jpg", String());
-    Serial.println("Got HTTP_GET on '/cloud_1.jpg'");
-  });
+  skyNetStruct->server->on("/cloud_1.jpg", HTTP_GET, [](AsyncWebServerRequest *request)
+                           {
+                             request->send(SPIFFS, "/cloud_1.jpg", String());
+                             Serial.println("Got HTTP_GET on '/cloud_1.jpg'");
+                           });
 
   // Responce for getting current system time.
-  skyNetStruct->server->on("/getCurrentSystemTime", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/plain", skyNetStruct->time_str.c_str());
-    Serial.println("Got HTTP_GET on '/getCurrentSystemTime'");
-  });
+  skyNetStruct->server->on("/getCurrentSystemTime", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             request->send_P(200, "text/plain", skyNetStruct->time_str.c_str());
+                             Serial.println("Got HTTP_GET on '/getCurrentSystemTime'");
+                           });
 
   /**
    * @brief Responce for setting current system time.
@@ -34,101 +38,113 @@ bool setUpServer(SkyNetStruct *skyNetStruct)
    * Hour:int
    * Minute:int
    */
-  skyNetStruct->server->on("/setCurrentSystemTime", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    // request->send_P(200, "text/plain", skyNetStruct->time_str.c_str());
-    AsyncWebParameter *p = request->getParam(0);
-    Serial.println("Got HTTP_GET on '/setCurrentSystemTime'");
-    if (p->name() == "hour")
-    {
-      Serial.print("Received new hour setting: ");
-      Serial.println(p->value());
-      skyNetStruct->rtc_timestamp.hour = p->value().toInt();
-      DS3231_set(skyNetStruct->rtc_timestamp);
-    }
-    if (p->name() == "minute")
-    {
-      Serial.print("Received new minute setting: ");
-      Serial.println(p->value());
-      skyNetStruct->rtc_timestamp.min = p->value().toInt();
-      DS3231_set(skyNetStruct->rtc_timestamp);
-    }
-    if (p->name() == "day")
-    {
-      Serial.print("Received new day setting: ");
-      Serial.println(p->value());
-      skyNetStruct->rtc_timestamp.mday = p->value().toInt();
-      DS3231_set(skyNetStruct->rtc_timestamp);
-    }
-    if (p->name() == "month")
-    {
-      Serial.print("Received new month setting: ");
-      Serial.println(p->value());
-      skyNetStruct->rtc_timestamp.mon = p->value().toInt();
-      DS3231_set(skyNetStruct->rtc_timestamp);
-    }
-  });
+  skyNetStruct->server->on("/setCurrentSystemTime", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             // request->send_P(200, "text/plain", skyNetStruct->time_str.c_str());
+                             AsyncWebParameter *p = request->getParam(0);
+                             Serial.println("Got HTTP_GET on '/setCurrentSystemTime'");
+                             if (p->name() == "hour")
+                             {
+                               Serial.print("Received new hour setting: ");
+                               Serial.println(p->value());
+                               skyNetStruct->rtc_timestamp.hour = p->value().toInt();
+                               DS3231_set(skyNetStruct->rtc_timestamp);
+                             }
+                             if (p->name() == "minute")
+                             {
+                               Serial.print("Received new minute setting: ");
+                               Serial.println(p->value());
+                               skyNetStruct->rtc_timestamp.min = p->value().toInt();
+                               DS3231_set(skyNetStruct->rtc_timestamp);
+                             }
+                             if (p->name() == "day")
+                             {
+                               Serial.print("Received new day setting: ");
+                               Serial.println(p->value());
+                               skyNetStruct->rtc_timestamp.mday = p->value().toInt();
+                               DS3231_set(skyNetStruct->rtc_timestamp);
+                             }
+                             if (p->name() == "month")
+                             {
+                               Serial.print("Received new month setting: ");
+                               Serial.println(p->value());
+                               skyNetStruct->rtc_timestamp.mon = p->value().toInt();
+                               DS3231_set(skyNetStruct->rtc_timestamp);
+                             }
+                             if (p->name() == "year")
+                             {
+                               Serial.print("Received new year setting: ");
+                               Serial.println(p->value());
+                               skyNetStruct->rtc_timestamp.year = p->value().toInt();
+                               DS3231_set(skyNetStruct->rtc_timestamp);
+                             }
+                           });
 
   /**
    * @brief Set new weather request from users html.
    * 
    */
-  skyNetStruct->server->on("/setCurrentSystemSystemWeather", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    AsyncWebParameter *p = request->getParam(0);
-    Serial.println("Got HTTP_GET on '/setCurrentSystemSystemWeather'");
-    Serial.println(p->name());
-    Serial.println(p->value());
-    skyNetStruct->current_weather = Weather::to_weather(p->value());
-  });
+  skyNetStruct->server->on("/setCurrentSystemSystemWeather", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             AsyncWebParameter *p = request->getParam(0);
+                             Serial.println("Got HTTP_GET on '/setCurrentSystemSystemWeather'");
+                             Serial.println(p->name());
+                             Serial.println(p->value());
+                             skyNetStruct->current_weather = Weather::to_weather(p->value());
+                           });
 
   // Responce for getting current Weather.
-  skyNetStruct->server->on("/getCurrentSystemWeather", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    request->send_P(200, "text/plain", Weather::to_string(skyNetStruct->current_weather).c_str());
-    Serial.println("Got HTTP_GET on '/getCurrentSystemWeather'");
-  });
+  skyNetStruct->server->on("/getCurrentSystemWeather", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             request->send_P(200, "text/plain", Weather::to_string(skyNetStruct->current_weather).c_str());
+                             Serial.println("Got HTTP_GET on '/getCurrentSystemWeather'");
+                           });
 
   // Responce for setting new leds brightness.
-  skyNetStruct->server->on("/setLedsBrightness", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    AsyncWebParameter *p = request->getParam(0);
-    Serial.println("Got HTTP_GET on '/setLedsBrightness'");
-    Serial.println(p->name());
-    Serial.println(p->value());
+  skyNetStruct->server->on("/setLedsBrightness", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             AsyncWebParameter *p = request->getParam(0);
+                             Serial.println("Got HTTP_GET on '/setLedsBrightness'");
+                             Serial.println(p->name());
+                             Serial.println(p->value());
 
-    if (!skyNetStruct->preferences.begin("SkyNetCloud"))
-    {
-      Serial.println("Could not initiate 'preference', on setLedsBrightness. Aborting");
-      request->send_P(200, "text/plain", "Error, server could not open preferences storace object.");
-      return;
-    }
+                             if (!skyNetStruct->preferences.begin("SkyNetCloud"))
+                             {
+                               Serial.println("Could not initiate 'preference', on setLedsBrightness. Aborting");
+                               request->send_P(200, "text/plain", "Error, server could not open preferences storace object.");
+                               return;
+                             }
 
-    ///< @brief Expected values between 0-255
-    auto brightness = p->value().toInt();
+                             ///< @brief Expected values between 0-255
+                             auto brightness = p->value().toInt();
 
-    // Store new value EEPROM.
-    skyNetStruct->preferences.putInt("LedBrightness", brightness);
-    skyNetStruct->preferences.end();
+                             // Store new value EEPROM.
+                             skyNetStruct->preferences.putInt("LedBrightness", brightness);
+                             skyNetStruct->preferences.end();
 
-    // Store new value in memory.
-    skyNetStruct->ledsSettings.leds_brightness = brightness;
-    skyNetStruct->ledsSettings.is_brightness_changed = true;
+                             // Store new value in memory.
+                             skyNetStruct->ledsSettings.leds_brightness = brightness;
+                             skyNetStruct->ledsSettings.is_brightness_changed = true;
 
-    request->send_P(200, "text/plain", "Brightness updated successfully");
-  });
+                             request->send_P(200, "text/plain", "Brightness updated successfully");
+                           });
 
   // Responce for getting current Weather.
-  skyNetStruct->server->on("/getLedsBrightness", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request) {
-    Serial.println("Got HTTP_GET on '/getLedsBrightness'");
-    if (!skyNetStruct->preferences.begin("SkyNetCloud"))
-    {
-      Serial.println("Could not initiate 'preference', on getLedsBrightness. Aborting");
-      request->send_P(200, "text/plain", "Error, server could not open preferences storace object.");
-      return;
-    }
-    skyNetStruct->ledsSettings.leds_brightness = skyNetStruct->preferences.getInt("LedBrightness");
-    skyNetStruct->ledsSettings.is_brightness_changed = true;
+  skyNetStruct->server->on("/getLedsBrightness", HTTP_GET, [skyNetStruct](AsyncWebServerRequest *request)
+                           {
+                             Serial.println("Got HTTP_GET on '/getLedsBrightness'");
+                             if (!skyNetStruct->preferences.begin("SkyNetCloud"))
+                             {
+                               Serial.println("Could not initiate 'preference', on getLedsBrightness. Aborting");
+                               request->send_P(200, "text/plain", "Error, server could not open preferences storace object.");
+                               return;
+                             }
+                             skyNetStruct->ledsSettings.leds_brightness = skyNetStruct->preferences.getInt("LedBrightness");
+                             skyNetStruct->ledsSettings.is_brightness_changed = true;
 
-    request->send_P(200, "text/plain", String(skyNetStruct->ledsSettings.leds_brightness).c_str());
-    skyNetStruct->preferences.end();
-  });
+                             request->send_P(200, "text/plain", String(skyNetStruct->ledsSettings.leds_brightness).c_str());
+                             skyNetStruct->preferences.end();
+                           });
 
   // Start server
   skyNetStruct->server->begin();
